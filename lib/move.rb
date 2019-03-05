79ea75263f7ad9f272bcf2e1327fbae344086dbc
Tymeckia Kendall <tykend@gmail.com>
2019-03-05 01:54:54 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index (user_input)
   user_input.to_i - 1
 end
 
-def move (index, current_player="X")
-  board [index.to_i] = current_player
+def move(index, token)
+  board [index.to_i] = token
 end
